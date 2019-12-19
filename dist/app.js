d9b74815d64ab2a29d4bcec527ce559dfd660797
Oliver Pope <popeoliv@msu.edu>
2019-12-18 20:13:02 -0500

    media queries bitches!

--- dist/app.js
+++ dist/app.js
@@ -1,1 +1,231 @@
-!function(e){var t={};function n(a){if(t[a])return t[a].exports;var r=t[a]={i:a,l:!1,exports:{}};return e[a].call(r.exports,r,r.exports,n),r.l=!0,r.exports}n.m=e,n.c=t,n.d=function(e,t,a){n.o(e,t)||Object.defineProperty(e,t,{enumerable:!0,get:a})},n.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},n.t=function(e,t){if(1&t&&(e=n(e)),8&t)return e;if(4&t&&"object"==typeof e&&e&&e.__esModule)return e;var a=Object.create(null);if(n.r(a),Object.defineProperty(a,"default",{enumerable:!0,value:e}),2&t&&"string"!=typeof e)for(var r in e)n.d(a,r,function(t){return e[t]}.bind(null,r));return a},n.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return n.d(t,"a",t),t},n.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},n.p="/",n(n.s=0)}([function(e,t,n){n(1),e.exports=n(3)},function(e,t,n){for(var a=n(2),r=document.querySelector("#app"),o=a.makeCalendar(),d=new Date,c=d.getFullYear(),u=d.getMonth()+1,i=u;i<12+u;i++){for(var l=new Date(c,i,0),s=a.makeMonth(l),m=1;m<l.getDate()+1;m++){var f=new Date(l.getFullYear(),l.getMonth(),m);1==m&&s.childNodes[2].appendChild(a.makeOffset(f.getDay()));var p=a.makeDay(f);s.childNodes[2].appendChild(p)}o.appendChild(s)}r.appendChild(o),a.getToday(d)},function(e,t){var n={makeCalendar:function(){var e=document.createElement("div");return e.classList.add("calendar"),e},makeMonth:function(e){var t=document.createElement("div");t.classList.add("month");var a=document.createElement("div");a.classList.add("month-name"),a.textContent=new Intl.DateTimeFormat("en-us",{month:"long",year:"numeric"}).format(e),t.appendChild(a),t.appendChild(n.addLabels());var r=document.createElement("div");return r.classList.add("month-months"),t.appendChild(r),t},makeDay:function(e){var t=document.createElement("div");return t.classList.add("date"),t.textContent=e.getDate(),t.dataset.date=e.toLocaleDateString("en-us",{year:"numeric",month:"numeric",day:"numeric"}),t},makeOffset:function(e){for(var t=document.createDocumentFragment(),n=0;n<e;n++){var a=document.createElement("div");a.classList.add("offset-block"),a.innerHTML="00",t.appendChild(a)}return t},addLabels:function(){var e=document.createElement("div");e.classList.add("days-labels");return["S","M","T","W","T","F","S"].forEach((function(t){var n=document.createElement("div");n.classList.add("label"),n.textContent=t,e.appendChild(n)})),e},getToday:function(e){var t=document.querySelector('[data-date="'.concat(e.toLocaleDateString("en-us",{year:"numeric",month:"numeric",day:"numeric"}),'"]'));return t.classList.add("today"),t}};e.exports=n},function(e,t){}]);
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "/";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./src/app.js":
+/*!********************!*\
+  !*** ./src/app.js ***!
+  \********************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+var ui = __webpack_require__(/*! ./ui */ "./src/ui.js");
+
+var app = document.querySelector('#app');
+var cal = ui.makeCalendar();
+var today = new Date();
+var year = today.getFullYear();
+var month = today.getMonth() + 1;
+
+for (var i = month; i < 12 + month; i++) {
+  var m = new Date(year, i, 0);
+
+  var _month = ui.makeMonth(m);
+
+  for (var j = 1; j < m.getDate() + 1; j++) {
+    var d = new Date(m.getFullYear(), m.getMonth(), j);
+
+    if (j == 1) {
+      _month.childNodes[2].appendChild(ui.makeOffset(d.getDay()));
+    }
+
+    var day = ui.makeDay(d);
+
+    _month.childNodes[2].appendChild(day);
+  }
+
+  cal.appendChild(_month);
+}
+
+app.appendChild(cal);
+ui.getToday(today);
+
+/***/ }),
+
+/***/ "./src/app.scss":
+/*!**********************!*\
+  !*** ./src/app.scss ***!
+  \**********************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+
+/***/ "./src/ui.js":
+/*!*******************!*\
+  !*** ./src/ui.js ***!
+  \*******************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+var ui = {
+  makeCalendar: function makeCalendar() {
+    var cal = document.createElement('div');
+    cal.classList.add('calendar');
+    return cal;
+  },
+  makeMonth: function makeMonth(month) {
+    var m = document.createElement('div');
+    m.classList.add('month');
+    var n = document.createElement('div');
+    n.classList.add('month-name');
+    n.textContent = new Intl.DateTimeFormat('en-us', {
+      month: "long",
+      year: "numeric"
+    }).format(month);
+    m.appendChild(n);
+    m.appendChild(ui.addLabels());
+    var g = document.createElement('div');
+    g.classList.add('month-months');
+    m.appendChild(g);
+    return m;
+  },
+  makeDay: function makeDay(date) {
+    var d = document.createElement('div');
+    d.classList.add('date');
+    d.textContent = date.getDate();
+    d.dataset.date = date.toLocaleDateString('en-us', {
+      year: 'numeric',
+      month: 'numeric',
+      day: 'numeric'
+    });
+    return d;
+  },
+  makeOffset: function makeOffset(offset) {
+    var o = document.createDocumentFragment();
+
+    for (var i = 0; i < offset; i++) {
+      var offsetBlock = document.createElement('div');
+      offsetBlock.classList.add('offset-block');
+      offsetBlock.innerHTML = "00";
+      o.appendChild(offsetBlock);
+    }
+
+    return o;
+  },
+  addLabels: function addLabels() {
+    var dayLabel = document.createElement('div');
+    dayLabel.classList.add('days-labels');
+    var labels = ['S', 'M', 'T', 'W', 'T', 'F', 'S'];
+    labels.forEach(function (l) {
+      var label = document.createElement('div');
+      label.classList.add('label');
+      label.textContent = l;
+      dayLabel.appendChild(label);
+    });
+    return dayLabel;
+  },
+  getToday: function getToday(today) {
+    var todayInCal = document.querySelector("[data-date=\"".concat(today.toLocaleDateString('en-us', {
+      year: 'numeric',
+      month: 'numeric',
+      day: 'numeric'
+    }), "\"]"));
+    todayInCal.classList.add('today');
+    return todayInCal;
+  }
+};
+module.exports = ui;
+
+/***/ }),
+
+/***/ 0:
+/*!*****************************************!*\
+  !*** multi ./src/app.js ./src/app.scss ***!
+  \*****************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+__webpack_require__(/*! /Users/oliver/Documents/Projects/calendar/src/app.js */"./src/app.js");
+module.exports = __webpack_require__(/*! /Users/oliver/Documents/Projects/calendar/src/app.scss */"./src/app.scss");
+
+
+/***/ })
+
+/******/ });
